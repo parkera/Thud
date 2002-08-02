26560459cad6a2702cb3c3d21b0fa948d87a90d9
Tony Parker <parkeras@gmail.com>
2002-08-02 23:26:02 +0000

    Files for new data package

---
+++ thud/btthud/data/MUHex.java
@@ -0,0 +1,54 @@
+//
+//  MUHex.java
+//  Thud
+//
+//  Created by Anthony Parker on Mon Dec 17 2001.
+//  Copyright (c) 2001-2002 Anthony Parker. All rights reserved.
+//  Please see LICENSE.TXT for more information.
+//
+//
+package btthud.data;
+
+public class MUHex
+{
+
+    char			terrain;
+    int			elevation;
+    
+    public MUHex()
+    {
+        // Default terrain is a level 0 unknown
+        terrain = '?';
+        elevation = 0;
+    }
+
+    public MUHex(char terrain, int elevation)
+    {
+        this.terrain = terrain;
+        this.elevation = elevation;
+    }
+
+    public int absElevation()
+    {
+        return Math.abs(elevation);
+    }
+
+    // ---------------
+
+    public char terrain() {
+        return terrain;
+    }
+
+    public int elevation() {
+        return elevation;
+    }
+
+    public void setTerrain(char t) {
+        terrain = t;
+    }
+
+    public void setElevation(int e) {
+        elevation = e;
+    }
+    
+}
