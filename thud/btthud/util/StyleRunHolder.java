49529f5dccaae06ff6b69636f0386f39b80ea6a9
Tony Parker <parkeras@gmail.com>
2002-08-02 23:23:49 +0000

    Files for util package

---
+++ thud/btthud/util/StyleRunHolder.java
@@ -0,0 +1,30 @@
+//
+//  StyleRunHolder.java
+//  Thud
+//
+//  Created by Anthony Parker on Sun Dec 30 2001.
+//  Copyright (c) 2001-2002 Anthony Parker. All rights reserved.
+//  Please see LICENSE.TXT for more information.
+//
+package btthud.util;
+
+public class StyleRunHolder {
+
+    public String		style;
+    public int			start;
+    public int			length;
+    
+    public StyleRunHolder()
+    {
+        style = "regular";
+        start = 0;
+        length = 0;
+    }
+
+    public StyleRunHolder(String style, int start, int length)
+    {
+        this.style = style;
+        this.start = start;
+        this.length = length;
+    }
+}
