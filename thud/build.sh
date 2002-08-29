3d5daeb65e40c6e46aa6e61dc9cef1a86799cc53
Tony Parker <parkeras@gmail.com>
2002-08-29 05:53:33 +0000

    Added this build script for unix/linux-type systems. Won't do much for windows, unfortunately.

---
+++ thud/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+##########
+# Written by YG, 8/6/2002
+# Modified by asp, 8/28/2002
+##########
+
+echo "Building Thud..."
+
+# Make sure we have our build directory
+mkdir -p build
+
+# Compile all files with .java in the name
+find . -name "*.java" | xargs javac -d build
+
+# Make the .jar file
+cd build
+jar cmf ../MANIFEST.MF Thud.jar btthud/
+
+# Done!
+echo "All done. To run Thud, change directory to 'build', then type:"
+echo "java -jar Thud"
+echo "You may want to remove the files in build/btthud."
+echo "Have a nice day."
+
+
