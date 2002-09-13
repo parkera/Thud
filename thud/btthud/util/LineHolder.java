ebbab55c8c7ceef644482ac3b8c0525121097366
Tony Parker <parkeras@gmail.com>
2002-09-13 19:04:08 +0000

    Added a little description of what's going on here.

--- thud/btthud/util/LineHolder.java
+++ thud/btthud/util/LineHolder.java
@@ -7,6 +7,12 @@
 //
 package btthud.util;
 
+/* The way this works is that MUParse uses the put() function to store a String in the LineHolder.
+ * Then MUConnection uses the get() function to get the string and parse it. Splitting this up allows
+ * us to use a Producer (MUConnection) / Consumer (MUParse) thread structure. Because of the wait() and
+ * notifyAll() calls, the MUParse thread isn't 'busy waiting', wasting CPU time and holding up other threads.
+ */
+
 public class LineHolder {
 
     String			s = null;
